e25704008186fd040f32edfd82abf2384ba75022
Sophie - Luise Heidig <sophie@MacganLeFay.local>
2024-09-17 15:31:03 +0200

    update git from local mac

--- bin/treeToCluster.py
+++ bin/treeToCluster.py
@@ -1,4 +1,4 @@
-from ete4 import Tree
+from ete4 import Tree,NodeStyle,TreeStyle
 import sys
 import numpy as np
 import pandas as pd
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 mytree=sys.argv[1] #"results_testing/example_filtered_NT_checked.afasta.treefile" 
 
-inputname=mytree.split('.')[0]
+inputname=mytree.split('/')[-1].split('.')[0]
 
 #read tree
 t = Tree(open(mytree),parser=1)
@@ -18,7 +18,7 @@ data = t.cophenetic_matrix()
 
 df=pd.DataFrame(data=data[0],     columns=data[1], index = data[1]) 
 df.reset_index(inplace=True)
-print (df)
+#print (df)
 #probably should have an alert if distance >10 //10x the other
 
 
@@ -27,36 +27,43 @@ a=np.array([np.array(xi) for xi in data[0]])
 distlist=a[np.triu_indices(a.shape[0],k=1)]
 
 Z=linkage(distlist, method='ward', metric='euclidean')
-print('ward')
-print(Z)
+#print('ward')
+#print(Z)
 #google if so else had cluster dist > node dist
 
 Z=linkage(distlist, method='single', metric='euclidean')
-print('single')
-print(Z)
+#print('single')
+#print(Z)
 
 #Z=ward(distlist) == Z=linkage(distlist, method='ward', metric='euclidean')
 Z=linkage(distlist, method='median', metric='euclidean')
-print('median')
-print(Z)
+#print('median')
+#print(Z)
 
 
 coeffs={}
 
-dfmax=round(df.max(numeric_only=True).max()*10)
-dfmin=round(df.mask(df==0).min(numeric_only=True).min()*10)
+dfmax=round(df.max(numeric_only=True).max()*100)
+dfmin=round(df.mask(df==0).min(numeric_only=True).min()*100)
 
-print('Testing distance tresholds ',dfmin/10, ' to ',dfmax/10 )
-for i in range(dfmin,dfmax,2):
-    i=i/10
+
+stepsize=3
+print('Testing distance tresholds ',dfmin/100, ' to ',dfmax/100 )
+for i in range(dfmin,dfmax,stepsize):#range(dfmin,dfmax,20):
+    i=i/100
 
     df['clusters']=fcluster(Z, i, criterion='distance')
     clusterlist=list(set(df['clusters'].to_list()))
-    if clusterlist != [1]:
+    print(i, clusterlist)
+    if 4 in clusterlist:
+        clust4hres=i
+    if len(clusterlist) > 2:
         sil=silhouette_score(a,df.clusters )
         coeffs[i]=sil
     else:
-        print('only one cluster at', i)
+        sil=silhouette_score(a,df.clusters )
+        coeffs[i]=sil
+        print('only two cluster at', i)
         break
 
 
@@ -64,11 +71,33 @@ for i in range(dfmin,dfmax,2):
 
 #get max score
 ideal_split_c=max(list(coeffs.values()))
+print(ideal_split_c)
 ideal_split=list(coeffs.keys())[list(coeffs.values()).index(ideal_split_c)]
 print("Ideal treshold: ", ideal_split)
 
-df['clusters']=fcluster(Z, ideal_split, criterion='distance')
-df['clusters'] = 'clade_'+df['clusters'].astype(str)
+
+localmax=[]
+scores=list(coeffs.values())
+for i in range(stepsize,len(scores)-stepsize):
+    coef=scores[i]
+    if coef >  scores[i-stepsize] and coef > scores[i+stepsize]:
+        localmax.append(list(coeffs.keys())[i])
+        localmax_split=list(coeffs.keys())[i]
+print(localmax)
+
+
+
+print("max threshold for 4 clusters:",clust4hres)
+
+
+df['clusters_globalMax']=fcluster(Z, ideal_split, criterion='distance')
+df['clusters_globalMax'] = 'clade_'+df['clusters_globalMax'].astype(str)
+
+df['clusters_4clades']=fcluster(Z, clust4hres, criterion='distance')
+df['clusters_4clades'] = 'clade_'+df['clusters_4clades'].astype(str)
+
+df['clusters_localMax']=fcluster(Z, localmax_split, criterion='distance')
+df['clusters_localMax'] = 'clade_'+df['clusters_localMax'].astype(str)
 
 #df['clusters_max4_cl']=fcluster(Z, 4, criterion='maxclust')
 
@@ -85,5 +114,50 @@ plt.savefig(inputname+"_dist_thres.pdf")
 
 
 
+
+clusteroptions=['clusters_4clades','clusters_localMax','clusters_globalMax']
+
+
+colors=['LightCoral','LightGrey','PeachPuff','LightCyan','AntiqueWhite','LightSalmon','Khaki','PaleVioletRed','Lavender','PaleGreen','Beige','PowderBlue']
+
+for option in clusteroptions:
+    clade_df=df.filter(items=['index',option])
+    cladel=clade_df.groupby(by=option)['index'].apply(list)
+    claded=cladel.to_dict()
+
+    colmap={}
+    for i in range(len(claded.keys())):
+        clade=list(claded.keys())[i]
+        if i >= len(colors):
+            if i == len(colors):
+                x=0
+            else:
+                x=x+1
+        elif i ==0:
+            x=len(colors)-1
+        else:
+            x=i
+        colmap[clade]=colors[x]
+
+    #print(colmap)
+
+    nt=t
+    for i in range(len(claded.keys())):
+
+        clade=list(claded.keys())[i]
+
+        leaves=list(claded.values())[i]
+
+        
+
+        print(clade,colmap[clade])
+        nst = NodeStyle()
+        nst["bgcolor"] = colmap[clade]
+        n = nt.common_ancestor(leaves)
+        n.set_style(nst)
+
+    ts = TreeStyle()
+    ts.mode = "c"
+    nt.show(tree_style=ts)
 
 
