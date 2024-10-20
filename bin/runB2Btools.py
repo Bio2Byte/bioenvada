58b3d210c4881e892c87856b2864cee640035169
vsc10579 <vsc10579@login2.cerberus.os>
2024-10-20 22:55:18 +0200

    robust against same seqs in msa

--- bin/runB2Btools.py
+++ bin/runB2Btools.py
@@ -2,6 +2,7 @@ import json
 from b2bTools.multipleSeq.Predictor import MineSuiteMSA
 import sys
 
+from Bio import SeqIO
 
 sequences=sys.argv[1]
 sequences_name=sys.argv[2]
@@ -10,6 +11,23 @@ predtypes=sys.argv[3].replace(' ','').split(',')
 
 print(predtypes)
 print ('import complete')
+
+
+
+#check if all seqs are the same, add trailing -
+seq_dict = {rec.id : str(rec.seq) for rec in SeqIO.parse(sequences, "fasta")}
+sameseqs=list(set(seq_dict.values()))
+print(sameseqs)
+if len(sameseqs) ==1:
+    print('this file contains only identical sequences')
+
+    with open (sequences, 'w') as m:
+            for key in seq_dict.keys():
+                row =  key+ '\n'+seq_dict[key]+'-'
+                m.write( '>'+row + '\n')   
+
+
+
 
 msaSuite = MineSuiteMSA()
 msaSuite.predictAndMapSeqsFromMSA(sequences, predTypes=predtypes)
